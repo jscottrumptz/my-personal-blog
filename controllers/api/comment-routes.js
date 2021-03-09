64f02915c95b78ca536867fb6803b5c2742dffb7
J Scott Rumptz <jscott@rumptz.tech>
2021-03-09 11:28:21 -0600

    comment routes created and tested in insomnia

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,9 +1,57 @@
 const router = require('express').Router();
+const { Comment } = require('../../models');
 
-// comment
+// custom authorization middleware
+const withAuth = require('../../utils/auth');
+
 router.get('/', (req, res) => {
-    // to console-log the session variables
-    console.log("COMMENT" + req.session);
-})
+    Comment.findAll({
+        comment_text: req.body.comment_text,
+        user_id: req.body.user_id,
+        post_id: req.body.post_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
+
+router.post('/', withAuth, (req, res) => {
+    // check the session
+  if (req.session) {
+    Comment.create({
+        comment_text: req.body.comment_text,
+        post_id: req.body.post_id,
+        // use the id from the session
+        user_id: req.session.user_id
+    })
+    .then(dbCommentData => res.json(dbCommentData))
+    .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+        });
+    }
+});
+
+router.delete('/:id', withAuth, (req, res) => {
+    Comment.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+    .then(dbPostData => {
+        // see if there is a matching id
+        if (!dbPostData) {
+            res.status(404).json({ message: 'No comment found with this id' });
+            return;
+        }
+        res.json(dbPostData);
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
